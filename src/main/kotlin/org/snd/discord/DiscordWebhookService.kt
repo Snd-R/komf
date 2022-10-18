86294f116bf04d4b6c01d225773604a63a01cacb
Snd-R <76580768+Snd-R@users.noreply.github.com>
2022-10-18 20:53:21 +0300

    limit embed description length to 4095

--- src/main/kotlin/org/snd/discord/DiscordWebhookService.kt
+++ src/main/kotlin/org/snd/discord/DiscordWebhookService.kt
@@ -14,28 +14,37 @@ private val logger = KotlinLogging.logger {}
 class DiscordWebhookService(
     private val webhooks: Collection<String>,
     private val discordClient: DiscordClient,
-    private val velocityEngine: VelocityEngine,
+    velocityEngine: VelocityEngine,
 ) {
+    private val template = velocityEngine.getTemplate("discordWebhook.vm")
 
     fun send(message: WebhookMessage) {
-        webhooks.map { discordClient.getWebhook(it) }.forEach { webhook ->
-            toRequest(message)?.let { discordClient.executeWebhook(webhook, it) }
-        }
+        webhooks.map { discordClient.getWebhook(it) }
+            .forEach { webhook ->
+                toRequest(message)
+                    .forEach { discordClient.executeWebhook(webhook, it) }
+            }
     }
 
-    private fun toRequest(message: WebhookMessage): WebhookExecuteRequest? {
-        val template = velocityEngine.getTemplate("discordWebhook.vm")
-        val writer = StringWriter()
-        template.merge(message.toVelocityContext(), writer)
-        val description = writer.toString()
+    private fun toRequest(message: WebhookMessage): Collection<WebhookExecuteRequest> {
+        val description = renderTemplate(message).take(4095)
+
         if (description.isBlank()) {
             logger.warn { "empty discord message for series ${message.series.name}. Skipping notification" }
-            return null
+            return emptyList()
         }
+
         val embed = Embed(
             description = description,
             color = "1F8B4C".toInt(16),
         )
-        return WebhookExecuteRequest(embeds = listOf(embed))
+        return listOf(WebhookExecuteRequest(embeds = listOf(embed)))
+    }
+
+    private fun renderTemplate(message: WebhookMessage): String {
+        return StringWriter().use {
+            template.merge(message.toVelocityContext(), it)
+            it.toString()
+        }
     }
 }
