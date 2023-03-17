4bf1ab218d3f98de3a55b7ab3aff64c9ec34be2c
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-03-17 13:58:05 +0300

    merge tags and genres option. closes #29

--- src/main/kotlin/org/snd/module/MediaServerModule.kt
+++ src/main/kotlin/org/snd/module/MediaServerModule.kt
@@ -15,6 +15,7 @@ import org.snd.config.KomgaConfig
 import org.snd.config.MetadataProcessingConfig
 import org.snd.config.MetadataUpdateConfig
 import org.snd.mediaserver.MediaServerClient
+import org.snd.mediaserver.MetadataMerger
 import org.snd.mediaserver.MetadataPostProcessor
 import org.snd.mediaserver.MetadataService
 import org.snd.mediaserver.MetadataUpdateMapper
@@ -263,6 +264,7 @@ class MediaServerModule(
             executor = metadataAggregationExecutor,
             metadataUpdateService = metadataUpdateService,
             seriesMatchRepository = seriesMatchRepository,
+            metadataMerger = MetadataMerger(mergeTags = config.mergeTags, mergeGenres = config.mergeGenres)
         )
     }
 
