8dbc7f7d9343556314c9e44d9f74d089de8880c9
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-05-12 15:40:48 +0300

    nullable images

--- src/main/kotlin/org/snd/metadata/providers/comicvine/model/ComicVineImage.kt
+++ src/main/kotlin/org/snd/metadata/providers/comicvine/model/ComicVineImage.kt
@@ -6,24 +6,24 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ComicVineImage(
     @Json(name = "icon_url")
-    val iconUrl: String,
+    val iconUrl: String?,
     @Json(name = "medium_url")
-    val mediumUrl: String,
+    val mediumUrl: String?,
     @Json(name = "screen_url")
-    val screenUrl: String,
+    val screenUrl: String?,
     @Json(name = "screen_large_url")
-    val screenLargeUrl: String,
+    val screenLargeUrl: String?,
     @Json(name = "small_url")
-    val smallUrl: String,
+    val smallUrl: String?,
     @Json(name = "super_url")
-    val superUrl: String,
+    val superUrl: String?,
     @Json(name = "thumb_url")
-    val thumbUrl: String,
+    val thumbUrl: String?,
     @Json(name = "tiny_url")
-    val tinyUrl: String,
+    val tinyUrl: String?,
     @Json(name = "original_url")
-    val originalUrl: String,
+    val originalUrl: String?,
     @Json(name = "image_tags")
-    val imageTags: String,
+    val imageTags: String?,
 )
 
