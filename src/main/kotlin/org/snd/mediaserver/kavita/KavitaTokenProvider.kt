09c96a43c620bc819fd996e25ec03ec702228605
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-06-02 17:17:39 +0300

    adjust token expiration time check

--- src/main/kotlin/org/snd/mediaserver/kavita/KavitaTokenProvider.kt
+++ src/main/kotlin/org/snd/mediaserver/kavita/KavitaTokenProvider.kt
@@ -4,7 +4,7 @@ import org.jose4j.jwt.consumer.JwtConsumerBuilder
 import org.snd.mediaserver.kavita.model.KavitaAccessToken
 import java.time.Clock
 import java.time.Instant
-import java.time.temporal.ChronoUnit.DAYS
+import java.time.temporal.ChronoUnit.HOURS
 import java.util.*
 import java.util.concurrent.locks.ReentrantLock
 import javax.crypto.spec.SecretKeySpec
@@ -32,14 +32,16 @@ class KavitaTokenProvider(
     }
 
     private fun updateAndGetToken(): KavitaAccessToken {
-        tokenLock.lock()
-        val lockedToken = kavitaToken
-        val freshToken = if (lockedToken == null || isExpired(lockedToken))
-            getFreshToken().also { kavitaToken = it }
-        else lockedToken
-        tokenLock.unlock()
+        try {
+            tokenLock.lock()
+            val lockedToken = kavitaToken
 
-        return freshToken
+            return if (lockedToken == null || isExpired(lockedToken))
+                getFreshToken().also { kavitaToken = it }
+            else lockedToken
+        } finally {
+            tokenLock.unlock()
+        }
     }
 
     private fun getFreshToken(): KavitaAccessToken {
@@ -50,6 +52,6 @@ class KavitaTokenProvider(
     }
 
     private fun isExpired(token: KavitaAccessToken): Boolean {
-        return clock.instant().isAfter(token.expiresAt.minus(1, DAYS))
+        return clock.instant().isAfter(token.expiresAt.minus(12, HOURS))
     }
 }
