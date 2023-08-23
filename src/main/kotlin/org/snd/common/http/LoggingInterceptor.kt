860a33c252d02114b8568e438617df5a292ff28e
Snd-R <76580768+snd-r@users.noreply.github.com>
2023-08-23 23:11:44 +0300

    do not log multipart bodies

--- src/main/kotlin/org/snd/common/http/LoggingInterceptor.kt
+++ src/main/kotlin/org/snd/common/http/LoggingInterceptor.kt
@@ -2,6 +2,7 @@ package org.snd.common.http
 
 import mu.KotlinLogging
 import okhttp3.Interceptor
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level
 
@@ -12,7 +13,12 @@ class LoggingInterceptor(level: Level = Level.BODY) : Interceptor {
     private val realInterceptor = HttpLoggingInterceptor { message -> logger.debug { message } }
         .setLevel(level)
 
-
-    override fun intercept(chain: Interceptor.Chain) = realInterceptor.intercept(chain)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        return if (chain.request().body?.contentType()?.type == "multipart") {
+            chain.proceed(chain.request())
+        } else {
+            realInterceptor.intercept(chain)
+        }
+    }
 }
 
