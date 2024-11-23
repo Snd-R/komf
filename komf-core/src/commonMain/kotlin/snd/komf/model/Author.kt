76543aeeb6934933968b5659bafca4c50b617dc8
Snd-R <76580768+snd-r@users.noreply.github.com>
2024-11-23 09:58:16 +0300

    Revert "Making role optional"
    This reverts commit 7941b6719e99220729680acf6a4712a4ec2cf85a.

--- komf-core/src/commonMain/kotlin/snd/komf/model/Author.kt
+++ komf-core/src/commonMain/kotlin/snd/komf/model/Author.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Author(
     val name: String,
-    val role: AuthorRole? = null
+    val role: AuthorRole
 )
 enum class AuthorRole {
     WRITER,
