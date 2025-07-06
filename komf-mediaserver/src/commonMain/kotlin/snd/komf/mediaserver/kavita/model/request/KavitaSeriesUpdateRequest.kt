ca0f23bce2e49bad8aed1ba1d90e37957db30e08
schen31 <shiqiu.chen@icloud.com>
2025-07-06 15:38:06 -0700

    Remove extra fields in KavitaSeries data model to resolve deserialization issue

--- komf-mediaserver/src/commonMain/kotlin/snd/komf/mediaserver/kavita/model/request/KavitaSeriesUpdateRequest.kt
+++ komf-mediaserver/src/commonMain/kotlin/snd/komf/mediaserver/kavita/model/request/KavitaSeriesUpdateRequest.kt
@@ -6,11 +6,9 @@ import snd.komf.mediaserver.kavita.model.KavitaSeriesId
 @Serializable
 data class KavitaSeriesUpdateRequest(
     val id: KavitaSeriesId,
-    val name: String,
     val localizedName: String? = null,
     val sortName: String,
     val coverImageLocked: Boolean,
-    val nameLocked: Boolean,
     val sortNameLocked: Boolean,
     val localizedNameLocked: Boolean
 )
