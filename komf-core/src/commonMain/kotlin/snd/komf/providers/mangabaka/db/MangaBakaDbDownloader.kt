dc42a2556d249f70708ed5d583ec8de5170d8cae
Snd-R <76580768+snd-r@users.noreply.github.com>
2025-07-22 23:51:38 +0300

    create directories before download

--- komf-core/src/commonMain/kotlin/snd/komf/providers/mangabaka/db/MangaBakaDbDownloader.kt
+++ komf-core/src/commonMain/kotlin/snd/komf/providers/mangabaka/db/MangaBakaDbDownloader.kt
@@ -29,6 +29,7 @@ import snd.komf.providers.mangabaka.db.MangaBakaDownloadProgress.FinishedEvent
 import snd.komf.providers.mangabaka.db.MangaBakaDownloadProgress.ProgressEvent
 import java.io.BufferedInputStream
 import java.nio.file.Path
+import kotlin.io.path.createDirectories
 import kotlin.io.path.deleteIfExists
 import kotlin.io.path.exists
 import kotlin.io.path.inputStream
@@ -75,6 +76,7 @@ class MangaBakaDbDownloader(
             dbMetadata.delete()
             databaseFile.deleteIfExists()
             databaseArchive.deleteIfExists()
+            databaseFile.createDirectories()
 
             downloadDatabaseArchive()
             extractDatabaseFile()
